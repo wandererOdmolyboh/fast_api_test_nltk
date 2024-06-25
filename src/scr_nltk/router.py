c5ffdb422adc771ea2751778fbaa0febba88a93c
dmolyboh <denys.molyboha@cosmonova.net>
2024-06-26 01:25:07 +0300

    add exception for endpoint ner

--- src/scr_nltk/router.py
+++ src/scr_nltk/router.py
@@ -29,15 +29,17 @@ async def pos_tag_text(data: Token):
 
 @router.post("/ner")
 async def ner_text(data: Token):
+    try:
+        tokens = nltk.word_tokenize(data.text)
+        tagged_tokens = nltk.pos_tag(tokens)
+        entities_tree = nltk.ne_chunk(tagged_tokens)
 
-    tokens = nltk.word_tokenize(data.text)
-    tagged_tokens = nltk.pos_tag(tokens)
-    entities_tree = nltk.ne_chunk(tagged_tokens)
+        entities = []
+        for subtree in entities_tree.subtrees():
+            entity_name = ' '.join(c[0] for c in subtree.leaves())
+            entity_type = subtree.label()
+            entities.append((entity_name, entity_type))
 
-    entities = []
-    for subtree in entities_tree.subtrees():
-        entity_name = ' '.join(c[0] for c in subtree.leaves())
-        entity_type = subtree.label()
-        entities.append((entity_name, entity_type))
-
-    return {"entities": entities}
+        return {"entities": entities}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
